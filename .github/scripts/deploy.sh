d2803f5a8839aac78a892912c3c5ba946c1f3304
Alvaro del Castillo San Felix <alvaro.delcastillo@smartprotection.com>
2021-11-21 12:31:09 +0100

    Update ssh key pair

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -7,7 +7,7 @@ unzip -o terraform_1.0.11_linux_amd64.zip -d ~
 cd terraform/pipeline
 ~/terraform init
 ~/terraform apply -auto-approve
-~/terraform output | grep '"' | awk -F '"' 'BEGIN {i=1; print "[cluster]"} {print "node0" i++ " ansible_host="$2}' > ../../.github/ansible/hosts.ini
+~/terraform output | grep '"' | awk -F '"' 'BEGIN {i=1; print "[cluster]"} {print "node0" i++ " ansible_host="$2}' > ../../terraform/ansible/hosts.ini
 cd ../..
 
 # Ansible configuration
@@ -17,11 +17,11 @@ mkdir ~/.ssh
 echo $VAULT_PASS > ~/vault_passowrd_file.txt
 
 ansible-vault decrypt \
-  --vault-password-file=~/vault_passowrd_file.txt .github/actions/deploy/id_rsa_devops_encrypted \
+  --vault-password-file=~/vault_passowrd_file.txt terraform/ansible/id_rsa_devops_encrypted \
   --output=~/.ssh/id_rsa
 
 rm ~/vault_passowrd_file.txt
 
-ansible-playbook -u ubuntu -i .github/ansible/hosts.ini terraform/ansible/swarm/deploy_app.yml
+ansible-playbook -u ubuntu -i terraform/ansible/hosts.ini terraform/ansible/swarm/deploy_app.yml
 
 docker ps
